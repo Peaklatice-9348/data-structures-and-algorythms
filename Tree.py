f2179b03ffc296b832335d1f99cc776ff6826c2b
Peaklatice-9348 <yusufjavaid.072012@gmail.com>
2026-04-09 18:12:09 +0100

    Add files via upload

--- Tree.py
+++ Tree.py
@@ -21,16 +21,40 @@ class binTree():
         if self.r != None:
             self.r.postorder()
         print(self.data)
+    def search(self,value):
+        
+        if value < self.data:
+            if self.l == None:
+                return False
+            return self.l.search(value)
+        elif value > self.data:
+            if self.r == None:
+                return False
+            return self.r.search(value)
+        elif value == self.data:
+            return True
+    def insersion(self,value):
+        if value < self.data and self.l !=None:
+            self.l.insersion(value)
+        elif value < self.data and self.l == None:
+            self.l = binTree(value)
+        elif value > self.data and self.r !=None:
+            self.r.insersion(value)
+        elif value > self.data and self.r ==None:
+            self.r = binTree(value)
 
-tree = binTree(3)
-tree.l = binTree(1)
-tree.r = binTree(2)
-tree.l.l = binTree(5)
-tree.l.r = binTree(4)
-tree.r.r = binTree(7)
-tree.r.l = binTree(8)
-tree.preorder()
-print('\n')
+
+
+tree = binTree(14)
+tree.l = binTree(2)
+tree.r = binTree(27)
+tree.l.r = binTree(11)
+tree.r.r = binTree(38)
+tree.r.l = binTree(16)
+tree.l.r.l = binTree(5)
+tree.l.r.r = binTree(13)
 tree.inorder()
-print('\n')
-tree.postorder()
+tree.insersion(20)
+tree.inorder()
+
+print(tree.search(13))
